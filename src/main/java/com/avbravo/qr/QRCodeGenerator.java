24313b6e4077b8bb13dcd10bede402ab7737f748
avbravo <avbravo@avbravo>
2018-08-27 19:06:23 -0500

    codigo qr

---
+++ src/main/java/com/avbravo/qr/QRCodeGenerator.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.qr;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ *
+ * @author avbravo
+ */
+public class QRCodeGenerator {
+    private static final String QR_CODE_IMAGE_PATH = "./MyQRCode.png";
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+}
+    
+     public static void main(String[] args) {
+        try {
+            generateQRCodeImage("Aristides villlarreal bravo", 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
---
+++ src/main/java/com/avbravo/qr/QRCodeReader.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.avbravo.qr;
+
+import com.google.zxing.BinaryBitmap;
+import com.google.zxing.LuminanceSource;
+import com.google.zxing.MultiFormatReader;
+import com.google.zxing.NotFoundException;
+import com.google.zxing.Result;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.common.HybridBinarizer;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ *
+ * @author avbravo
+ */
+public class QRCodeReader {
+    private static String decodeQRCode(File qrCodeimage) throws IOException {
+        BufferedImage bufferedImage = ImageIO.read(qrCodeimage);
+        LuminanceSource source = new BufferedImageLuminanceSource(bufferedImage);
+        BinaryBitmap bitmap = new BinaryBitmap(new HybridBinarizer(source));
+
+        try {
+            Result result = new MultiFormatReader().decode(bitmap);
+            return result.getText();
+        } catch (NotFoundException e) {
+            System.out.println("There is no QR code in the image");
+            return null;
+        }
+    }
+    public static void main(String[] args) {
+        try {
+            File file = new File("MyQRCode.png");
+            String decodedText = decodeQRCode(file);
+            if(decodedText == null) {
+                System.out.println("No QR Code found in the image");
+            } else {
+                System.out.println("Decoded text = " + decodedText);
+            }
+        } catch (IOException e) {
+            System.out.println("Could not decode QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
